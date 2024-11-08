43484f2a1c54972c8703b9ff0007cfbf09e0e519
mendoza <Mendoza210727>
2024-11-08 01:57:58 -0400

    tabla escalas

--- DWbd/DB_Proyecto_soporte/BD_Multidimencional/DW_escalas.sql
+++ DWbd/DB_Proyecto_soporte/BD_Multidimencional/DW_escalas.sql
@@ -23,17 +23,22 @@ create table DIM_tiempo(
     hora_salida TIME,
     hora_llegada TIME,
 )
+create table DIM_duracion_escala(
+    id_duracion_escala INT IDENTITY(1,1) PRIMARY KEY,
+    duracion INT,
+)
 create table FACT_escalas(
     id_ INT IDENTITY (1,1) PRIMARY KEY,
-    duracion INT,
     id_aerolinea INT, 
     id_aeronave INT,
     id_ciudad INT,
     id_aeropuerto INT,
     id_tiempo INT,
+    id_duracion_escala INT,
     FOREIGN KEY (id_aerolinea) REFERENCES DIM_aerolinea (id_aerolinea),
     FOREIGN KEY (id_aeronave) REFERENCES  DIM_aeronave(id_aeronave),
     FOREIGN KEY (id_ciudad) REFERENCES  DIM_ciudad(id_ciudad),
     FOREIGN KEY (id_aeropuerto) REFERENCES  DIM_aeropuerto(id_aeropuerto),
-    FOREIGN KEY (id_tiempo) REFERENCES  DIM_tiempo(id_tiempo)
+    FOREIGN KEY (id_tiempo) REFERENCES  DIM_tiempo(id_tiempo),
+    FOREIGN KEY (id_duracion_escala) REFERENCES  DIM_duracion_escala(id_duracion_escala)
 )
--- DWbd/DB_Proyecto_soporte/BD_Multidimencional/DW_mantenimiento.sql
+++ DWbd/DB_Proyecto_soporte/BD_Multidimencional/DW_mantenimiento.sql
@@ -10,10 +10,6 @@ create table DIM_aerolinea(
     id_aerolinea INT IDENTITY(1,1) PRIMARY KEY,
     nombre_aerolinea VARCHAR(100)
 )
-create table DIM_repuesto(
-    id_repuesto INT IDENTITY(1,1) PRIMARY KEY,
-    nombre_repuesto VARCHAR(100)
-)
 create table DIM_tipo_mantenimiento(
     id_tipo_mantenimiento INT IDENTITY(1,1) PRIMARY KEY,
     tipo_mantenimiento VARCHAR(100)
@@ -23,28 +19,26 @@ create table DIM_empleado(
     nombre_empleado VARCHAR(100)
 )
 create table DIM_tiempo(
-    id_tiempo INT IDENTITY(1,1) PRIMARY KEY,
+    id_fecha  INT IDENTITY(1,1) PRIMARY KEY,
     fecha DATE,
+    cantidad_dias_mantenimiento INT,
     dia varchar(100),
     mes varchar(100),
     ano varchar(100),
     trimestre varchar(100),
-    semestre varchar(100),
-    duracio_mantenimiento INT
+    semestre varchar(100)
 )
 create table FACT_mantenimientos(
     id_mantenimientos INT IDENTITY(1,1) PRIMARY KEY,
     monto INT,
     id_aeronave INT,
     id_aerolinea INT,
-    id_repuesto INT,
     id_tipo_mantenimiento INT,
     id_empleado INT,
-    id_tiempo INT,
+    id_fecha  INT,
     FOREIGN KEY(id_aeronave) REFERENCES  DIM_aeronave(id_aeronave),
     FOREIGN KEY(id_aerolinea) REFERENCES DIM_aerolinea (id_aerolinea),
-    FOREIGN KEY(id_repuesto) REFERENCES  DIM_repuesto(id_repuesto),
     FOREIGN KEY(id_tipo_mantenimiento) REFERENCES  DIM_tipo_mantenimiento(id_tipo_mantenimiento),
     FOREIGN KEY(id_empleado) REFERENCES  DIM_empleado(id_empleado),
-    FOREIGN KEY(id_tiempo) REFERENCES  DIM_tiempo(id_tiempo)
+    FOREIGN KEY (id_fecha) REFERENCES DIM_tiempo(id_fecha)
 )
